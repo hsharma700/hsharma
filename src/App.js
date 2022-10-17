d4b27b286eb1acb59c1c4811586a32f9e4399e9d
Hsharma <himanshukrazzzy@gmail.com>
2022-10-17 12:50:25 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { BrowserRouter, Routes, Route, useLocation , useNavigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
